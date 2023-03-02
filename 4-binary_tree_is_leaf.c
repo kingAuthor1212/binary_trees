a88d0c4ec614b84a463da699deb61ff8d94e209d
kingAutgor1212 <kingauthor1212@gmail.com>
2023-03-01 23:25:27 -0800

    binary_trees

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -5,11 +5,11 @@
  * @node: node to study
  * Return: 1 if it is a leaf or 0 if it is not
  */
-int binary_trees_is_leaf(const binary_tree_t *node)
+int binary_tree_is_leaf(const binary_tree_t *node)
 {
 	if (node == NULL)
 		return (0);
-	if (node->left == NULL & node-> right == NULL)
+	if (node->left == NULL && node->right == NULL)
 		return (1);
 	return (0);
 }
